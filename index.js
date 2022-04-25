0dd1e6869cc100821e2da3ae89372aeabe61f767
Kevin Nielsen <knielsen24@gmail.com>
2022-04-25 15:27:14 -0700

    commit tree a99c9742e515cfcc978a6092abbfd7f93162562a

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
-// Write your code here
+const num1 = 2;
+const num2 = 31;
+
+const multiply = num1 * num2;
+console.log(multiply);
+
+
+const random = Math.floor(Math.random() * 1000) + 1;
+console.log(random);
+
+
+const num3 = 12;
+const num4 = 3;
+const mod = num3 / num4;
+
+console.log(mod);
+
+const max = Math.max(1, 3, 20);
+console.log(max);
